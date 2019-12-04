26f980dbc93f8fd8f07f6af3e930bb703a1e2d72
Age <ykjage@gmail.com>
2019-12-04 19:57:18 +0800

    .

--- src/components/urban/Scene.js
+++ src/components/urban/Scene.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react'
 import { Scene, PointLayer,LineLayer } from '@antv/l7';
 import { GaodeMap } from '@antv/l7-maps';
 import * as d3 from 'd3'
-import centerCsv from '../../data/center.csv'
 
 
-class Map extends Component {
+// 采用七牛云 存储 
+// 注意 30 天绑定域名会变化
+const centerCsvUrl = 'http://q1vcletmu.bkt.clouddn.com/center7.csv'
+const centerRoadUrl = 'http://q1vcletmu.bkt.clouddn.com/road_400_f.csv'
+const trajsUrl = 'http://q1vcletmu.bkt.clouddn.com/new_trajs.csv'
+
+
+
+class MyMap extends Component {
   state = {
     mapLoaded: false,
     scene: null
@@ -25,11 +32,11 @@ class Map extends Component {
     this.scene = new Scene({
       id: 'map',
       map: new GaodeMap({
-      pitch: 90,
+      // pitch: 90,
       type: 'amap',
       style: 'light',
       center: [ 114.5082664490,38.0413316426 ],
-      zoom: 13, 
+      zoom: 13.2, 
       token: '49aa9ff2c3f76f61ddd2d9b1302ebbd3'
       }),
     })
@@ -40,17 +47,16 @@ class Map extends Component {
 
   drawPoints(){
     let self = this
-
-    d3.csv(centerCsv , (row)=>{
-      return {
+    d3.csv(centerCsvUrl , (row)=>{
+      let p = {
         'lng' :  +row['longitude'],
         'lat' :  +row['latitude'],
       }
+      return p 
     })
     .then(data => {
-      console.log(data.length)
+      console.log("卡口数量",data.length)
       const pointLayer = new PointLayer({})
-      
       pointLayer
         .source(data, {
           parser: {
@@ -72,58 +78,66 @@ class Map extends Component {
   }
 
   drawLines(){
-    let self = this
 
-    d3.csv(centerCsv , (row)=>{
-      return {
+    let self = this
+    const centerMap = new Map()
+    const lines = []
+
+    d3.csv(centerCsvUrl , (row)=>{
+      let p =  {
         'lng' :  +row['longitude'],
         'lat' :  +row['latitude'],
+        'id'  :  +row['id']
       }
+      return p 
     })
-    .then(data => {
-
-      let points = data,
-          pNum = points.length,
-          lines = [],
-          lNum = 5000
-
-      for(let i = 0;i < lNum;i++){
-         let s_n = Math.floor(Math.random()*pNum),
-             e_n = Math.floor(Math.random()*pNum),
-             s_p = points[s_n],
-             e_p = points[e_n]
-          lines.push({
-            lng1 : s_p['lng'],
-            lat1 : s_p['lat'],
-            lng2 : e_p['lng'],
-            lat2 : e_p['lat']
-          })
-      }
-      console.log(lines.length)
-
-      const layer = new LineLayer({})
+    .then(centers => {
       
-      layer
-        .source(lines, {
-          parser: {
-            type:'json',
-            x: 'lng1',
-            y: 'lat1',
-            x1: 'lng2',
-            y1: 'lat2'
-          }
-        })
-        .size(1)
-        .shape('arc3d')
-        .color('#5CCEA1')
-        .style({
-          opacity: 0.4,
-          blur: 0.2
-        });
+      centers.map((center)=>{
+         centerMap.set(center.id , center)
+      })
+      console.log(centerMap)
 
-        self.scene.addLayer(layer);
+      d3.csv(centerRoadUrl)
+        .then((roadData)=>{
+
+            roadData.map((road)=>{
+              let s = road['v1'],
+                  e = road['v2'],
+                  s_p = centerMap.get(+s),
+                  e_p = centerMap.get(+e)
+              lines.push({
+                lng1 : s_p['lng'],
+                lat1 : s_p['lat'],
+                lng2 : e_p['lng'],
+                lat2 : e_p['lat']
+              })
+            })
+
+            console.log("道路数量",lines.length)
+
+            const layer = new LineLayer({})
+            layer
+              .source(lines, {
+                parser: {
+                  type:'json',
+                  x: 'lng1',
+                  y: 'lat1',
+                  x1: 'lng2',
+                  y1: 'lat2'
+                }
+              })
+              .size(1)
+              .shape('arc3d')
+              .color('#722ed1')
+              .style({
+                opacity: 0.85,
+              });
+            self.scene.addLayer(layer);
+        })
     });
   }
+
   render() {
     return (
       <div
@@ -142,4 +156,4 @@ class Map extends Component {
     )
   }
 }
-export default Map
+export default MyMap
