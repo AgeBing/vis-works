8adbd4d8fdcc156fc6a79d2bc82e943af58b511d
Age <ykjage@gmail.com>
2019-12-05 09:46:26 +0800

    切换 L7 至 v1.4.7

--- docs/README.md
+++ docs/README.md
@@ -5,4 +5,6 @@
 - [`antd`](https://ant.design/docs/react/introduce-cn)  UI组件      
 - `antv g2` 、[`bizcharts`](https://bizcharts.net/products/bizCharts/docs/start)  图表可视化
 - [`antv l7`](https://antv-l7.gitee.io/zh/docs/api/l7)  地理可视化
+	- [版本1.x](https://antv-2018.alipay.com/zh-cn/l7/1.x/demo/index.html) 
 - [`docsify`](https://docsify.js.org/#/zh-cn/quickstart) 文档构建
+- [`七牛云`](https://www.qiniu.com/)  数据存储
--- src/components/urban/Scene.js
+++ src/components/urban/Scene.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react'
-import { Scene, PointLayer,LineLayer } from '@antv/l7';
-import { GaodeMap } from '@antv/l7-maps';
 import * as d3 from 'd3'
-
+import * as L7 from '@antv/l7';
 
 // 采用七牛云 存储 
 // 注意 30 天绑定域名会变化
 const centerCsvUrl = 'http://q1vcletmu.bkt.clouddn.com/center7.csv'
 const centerRoadUrl = 'http://q1vcletmu.bkt.clouddn.com/road_400_f.csv'
-const trajsUrl = 'http://q1vcletmu.bkt.clouddn.com/new_trajs.csv'
-
-
+const trajsUrl = "http://q1vcletmu.bkt.clouddn.com/new_trajs.csv"
 
 class MyMap extends Component {
   state = {
-    mapLoaded: false,
-    scene: null
   }
  
   constructor(props) {
@@ -26,59 +20,54 @@ class MyMap extends Component {
     this.createInstance()
   }
 
-
-  createInstance() {
+  createInstance(){
     let self = this
-    this.scene = new Scene({
+    var scene = new L7.Scene({
       id: 'map',
-      map: new GaodeMap({
-      // pitch: 90,
-      type: 'amap',
-      style: 'light',
-      center: [ 114.5082664490,38.0413316426 ],
-      zoom: 13.2, 
-      token: '49aa9ff2c3f76f61ddd2d9b1302ebbd3'
-      }),
-    })
-
-    self.drawPoints()
-    self.drawLines()
+      mapStyle: 'light', // 样式URL
+      center: [114.5082664490,38.0413316426],
+      // center: [102.602992, 23.107329],
+      pitch: 15,
+      zoom: 13.2
+    });
+    this.scene = scene
+    scene.on('loaded', function() {
+        self.drawPoints()
+        self.drawLines()
+    });
   }
-
   drawPoints(){
-    let self = this
-    d3.csv(centerCsvUrl , (row)=>{
-      let p = {
-        'lng' :  +row['longitude'],
-        'lat' :  +row['latitude'],
-      }
-      return p 
-    })
-    .then(data => {
-      console.log("卡口数量",data.length)
-      const pointLayer = new PointLayer({})
-      pointLayer
-        .source(data, {
-          parser: {
-            type:'json',
-            x : 'lng',
-            y : 'lat'
+      let self = this
+      d3.csv(centerCsvUrl , (row)=>{
+        let p = {
+          'lng' :  +row['longitude'],
+          'lat' :  +row['latitude'],
+        }
+        return p 
+      })
+      .then(data => {
+        self.scene.PointLayer({
+          zIndex: 2
+        })
+        .source(data,{
+          parser:{
+              type:'json',
+              x : 'lng',
+              y : 'lat'
           }
         })
         .shape('circle')
-        .size(4)
+        .size(4) 
+        .active(true)
         .color('#5B8FF9')
         .style({
           opacity: 0.3,
           strokeWidth: 1
         })
-
-        self.scene.addLayer(pointLayer);
-    });
+        .render();
+      });
   }
-
   drawLines(){
-
     let self = this
     const centerMap = new Map()
     const lines = []
@@ -90,52 +79,47 @@ class MyMap extends Component {
         'id'  :  +row['id']
       }
       return p 
-    })
-    .then(centers => {
-      
+    }).then(centers => {
       centers.map((center)=>{
          centerMap.set(center.id , center)
       })
-      console.log(centerMap)
-
-      d3.csv(centerRoadUrl)
-        .then((roadData)=>{
-
-            roadData.map((road)=>{
-              let s = road['v1'],
-                  e = road['v2'],
-                  s_p = centerMap.get(+s),
-                  e_p = centerMap.get(+e)
-              lines.push({
-                lng1 : s_p['lng'],
-                lat1 : s_p['lat'],
-                lng2 : e_p['lng'],
-                lat2 : e_p['lat']
-              })
+      d3.csv(centerRoadUrl).then((roadData)=>{
+          roadData.map((road)=>{
+            let s = road['v1'],
+                e = road['v2'],
+                s_p = centerMap.get(+s),
+                e_p = centerMap.get(+e)
+            lines.push({
+              lng1 : s_p['lng'],
+              lat1 : s_p['lat'],
+              lng2 : e_p['lng'],
+              lat2 : e_p['lat']
             })
+          })
+          console.log("道路数量",lines.length)
 
-            console.log("道路数量",lines.length)
+          self.scene.LineLayer({
+            zIndex: 3
+          })
+          .source(lines, {
+            parser: {
+              type:'json',
+              x: 'lng1',
+              y: 'lat1',
+              x1: 'lng2',
+              y1: 'lat2'
+            }
+          })
+          .shape('line')
+          .size(1)
+          .color('#722ed1')
+          .style({
+            opacity: 0.85,
+          })
+          .render();
 
-            const layer = new LineLayer({})
-            layer
-              .source(lines, {
-                parser: {
-                  type:'json',
-                  x: 'lng1',
-                  y: 'lat1',
-                  x1: 'lng2',
-                  y1: 'lat2'
-                }
-              })
-              .size(1)
-              .shape('arc3d')
-              .color('#722ed1')
-              .style({
-                opacity: 0.85,
-              });
-            self.scene.addLayer(layer);
         })
-    });
+    })
   }
 
   render() {
